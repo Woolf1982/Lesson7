c6378ffc4ed0783a973a7fbb762729d369e406f3
Petr Volkov <118010969+Woolf1982@users.noreply.github.com>
2022-12-01 23:00:38 +0300

    Ex052

---
+++ Ex052/Program.cs
@@ -0,0 +1,58 @@
+﻿/* Задача 52. 
+Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+*/
+
+Console.Write("Enter the number of row: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter the number of column: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+void FillArrayRandomNumbers(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(0, 10);
+        }
+    }
+}
+
+void PrintArray(int[,] array)
+{
+
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        Console.Write("[ ");
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.Write("]");
+        Console.WriteLine("");
+    }
+}
+
+int[,] numbers = new int[n, m];
+FillArrayRandomNumbers(numbers);
+
+Console.WriteLine("\n");
+for (int j = 0; j < numbers.GetLength(1); j++)
+{
+    double avarage = 0;
+    for (int i = 0; i < numbers.GetLength(0); i++)
+    {
+        avarage = (avarage + numbers[i, j]);
+    }
+    avarage = avarage / n;
+    Console.Write(avarage + "; ");
+}
+Console.WriteLine("<- Arithmetic mean of each array column");
+Console.WriteLine();
+PrintArray(numbers);
